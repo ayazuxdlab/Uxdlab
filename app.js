c7535fe40ef2b68e9262649a70320b46732ad14f
ayazuxdlab <ayaz.ali@uxdlab.com>
2026-01-05 15:59:25 +0530

    Initial commit from server

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Node.js server is running successfully");
+});
+
+app.listen(3000, "0.0.0.0", () => {
+    console.log("Server started on port 3000");
+});
---
+++ deploy.bat
@@ -0,0 +1,18 @@
+@echo off
+echo === DEPLOY STARTED ===
+
+set PATH="C:\Program Files\nodejs"; "C:\Program Files\Git\cmd";%PATH%
+
+cd /d C:\Uxdlab_App
+IF ERRORLEVEL 1 (
+    echo FAILED to change directory
+    exit /b 1
+)
+
+"C:\Program Files\Git\cmd\git.exe" pull origin main
+
+"C:\Program Files\nodejs\npm.cmd" install --production
+
+C:\nssm-2.24\win64\nssm.exe restart uxdlab-app
+
+echo === DEPLOY FINISHED ===

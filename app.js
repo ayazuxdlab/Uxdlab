054081a87552a42c7f30efaf16f15bae1bdd1032
ayazuxdlab <ayaz.ali@uxdlab.com>
2026-01-07 11:53:13 +0530

    Update deploy version to 1.0.1

--- app.js
+++ app.js
@@ -2,10 +2,9 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("Node.js server is running successfully");
+    res.send("UXDLAB AUTO DEPLOY VERSION 1.0.1");
 });
 
 app.listen(3000, "0.0.0.0", () => {
     console.log("Server started on port 3000");
-    console.log("AUTO DEPLOY TEST SUCCESS");
 });
